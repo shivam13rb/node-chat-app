738c9aed6094efd5c312443d745e1dedf6f4f405
shivam13rb <shivam13rb@gmail.com>
2018-07-06 11:54:18 +0530

    Init

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const express = require('express');
 // path.join gives the correct path of public directory
 const publicPath = path.join(__dirname,'../public');
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 var app = express();
 
 app.use(express.static(publicPath));
