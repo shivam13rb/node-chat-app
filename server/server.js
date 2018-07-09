82688b3033699bc40a9f132180cf62c8f71fd849
shivam13rb <shivam13rb@gmail.com>
2018-07-09 18:27:18 +0530

    Add css for chat page

--- server/server.js
+++ server/server.js
@@ -28,7 +28,7 @@ socket.broadcast.emit('newMessage',generateMessage('Admin','New user created'));
 socket.on('createMessage',(message, callback) => {
         console.log('createMessage',message);
   io.emit('newMessage',generateMessage(message.from,message.text));
-callback('This is from the server');
+callback();
 });
 
 socket.on('createLocationMessage',(coords) => {
