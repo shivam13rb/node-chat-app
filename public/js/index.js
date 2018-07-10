fe589abeedaa36cfcd056d2681cb5db44deef0f1
shivam13rb <shivam13rb@gmail.com>
2018-07-10 16:02:45 +0530

    Scroll to bottom if user is close to buttom

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,20 @@
 var socket =  io();
+
+function scrollToBottom() {
+       //Selectors
+       var messages = jQuery('#messages');
+       var newMessage = messages.children('li:last-child')
+       //heights
+       var clientHeight = messages.prop('clientHeight');
+       var scrollTop = messages.prop('scrollTop');
+       var scrollHeight = messages.prop('scrollHeight');
+       var newMessageHeight = newMessage.innerHeight();
+       var lastMessageHeight = newMessage.prev().innerHeight();
+
+       if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
+           messages.scrollTop(scrollHeight);
+       }
+}
 socket.on('connect',function() {
     console.log('connected to server');
 
@@ -24,7 +40,7 @@ socket.on('newMessage',function(message) {
         createdAt: formattedTime
     });
     jQuery('#messages').append(html);
-    
+    scrollToBottom();
 //  var li = jQuery('<li></li>');
 //  li.text(`${message.from} ${formattedTime}: ${message.text}`);
 
@@ -40,7 +56,7 @@ socket.on('newLocationMessage',function(message){
       createdAt: formattedTime
   });
   jQuery('#messages').append(html);
-  
+  scrollToBottom();
   // var li = jQuery('<li></li>');
     // var a = jQuery('<a target = "_blank">My current location</a>')
     // li.text(`${message.from} ${formattedTime}:`);
